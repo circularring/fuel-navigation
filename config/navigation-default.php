e87d80c555d7c3beabecea82b13f1444b21f4893
Circularring <h.aso.113@gmail.com>
2013-01-31 23:44:39 +0900

    Update navigation-default.php config

--- config/navigation-default.php
+++ config/navigation-default.php
@@ -5,6 +5,27 @@ return array(
 		'title'       => 'title',
 		'description' => 'description',
 		'keywords'    => 'keywords',
-		'pages'       => array(),
+		'pages'       => array(
+			'/page1' => array(
+				'label'       => 'Home',
+				'title'       => 'title',
+				'description' => 'description',
+				'keywords'    => 'keywords',
+			),
+			'/page2' => array(
+				'label'       => 'Home',
+				'title'       => 'title',
+				'description' => 'description',
+				'keywords'    => 'keywords',
+				'pages' => array(
+					'/page2/page3' => array(
+						'label'       => 'Home',
+						'title'       => 'title',
+						'description' => 'description',
+						'keywords'    => 'keywords',
+					),
+				),
+			),
+		),
 	),
 );
